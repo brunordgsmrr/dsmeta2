ea55a841136a1051d408eac119df76aeddd98d2c
Bruno Rodrigues <bruno_rdgs_@hotmail.com>
2023-07-26 22:20:12 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,14 +1,26 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './style.css'
+import axios from "axios";
+import icon from "../../assets/img/notification-icon.svg";
+import "./style.css";
+import { BASE_URL } from "../../utils/request";
 
-function NotificationButton() {
-  return (
-    <>
-      <div className="dsmeta-red-btn">
-        <img src={icon} alt="Notificar" />
-      </div>
-    </>
-  );
+type Props = {
+	saleId: number;
+};
+
+function handleClick(id: number) {
+	axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+		console.log("Sucesso");
+	});
+}
+
+function NotificationButton({ saleId }: Props) {
+	return (
+		<>
+			<div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+				<img src={icon} alt="Notificar" />
+			</div>
+		</>
+	);
 }
 
 export default NotificationButton;
