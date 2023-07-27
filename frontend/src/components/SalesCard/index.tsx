aadcbb704e4efd3b5e041f27b75bcb416b3272f1
Bruno Rodrigues <bruno_rdgs_@hotmail.com>
2023-07-26 22:06:43 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,12 +17,16 @@ function SalesCard() {
 	const [sales, setSales] = useState<Sale[]>([]);
 
 	useEffect(() => {
+
+		const dmin = minDate.toISOString().slice(0, 10);
+		const dmax = maxDate.toISOString().slice(0, 10);
+		console.log(dmin);
 		axios
-			.get(`${BASE_URL}/sales?minDate=2020-01-01&maxDate=2023-01-01`)
+			.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
 			.then((response) => {
 				setSales(response.data.content);
 			});
-	}, []);
+	}, [minDate, maxDate]);
 
 	return (
 		<>
