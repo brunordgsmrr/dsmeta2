6aa5917b936325dc310cd58b4a13df13a57c094e
Bruno Rodrigues <bruno_rdgs_@hotmail.com>
2023-07-26 22:38:12 -0300

    Fixed yarn build

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -10,7 +10,8 @@ type Props = {
 
 function handleClick(id: number) {
 	axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
-		toast.info("SMS enviado")
+		response;
+		toast.info("SMS enviado para");
 	});
 }
 
